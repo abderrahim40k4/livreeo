004a30544e9b323ff701c0820e6e52194a361448
Younes Id Talab <113123436+younesidt@users.noreply.github.com>
2024-05-31 02:09:51 +0000

    axios update

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,9 +2,8 @@ import Axios from "axios";
 
 const axios = Axios.create({
     baseURL: 'https://livreeo.ma/api/public',
-    timeout: 60000,
     headers: {
-        Accept: 'application/json'
+        Accept: 'application/json',
     },
 });
 
